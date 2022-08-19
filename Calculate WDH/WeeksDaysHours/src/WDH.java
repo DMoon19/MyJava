f70fb875cc4809bc3460a08ec5a462fedce3496e
David Garcia <davidgarciasanchez09@gmail.com>
2022-08-18 19:21:08 -0500

    App to Translate hours into WDH

---
+++ Calculate WDH/WeeksDaysHours/src/WDH.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WDH {
+    public static void main(String[] args) throws Exception {
+    
+        Scanner sc=new Scanner(System.in);
+
+        int H, w,d,h;
+
+        System.out.println("Digite el número de horas: ");
+        H=sc.nextInt();
+
+        w=H/168;
+        d=H%168/24;
+        h=H%24;
+
+        System.out.println("\n-El equivalente es: ");
+        System.out.println("semanas: "+w);
+        System.out.println("días: "+d);
+        System.out.println("horas: "+h);
+    }
+}
