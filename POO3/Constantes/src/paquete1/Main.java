0ba2f14cb8446e009442b16355a50719682605fc
David Garcia <davidgarciasanchez09@gmail.com>
2022-08-23 12:29:51 -0500

    Const

---
+++ POO3/Constantes/src/paquete1/Main.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Main {
+    public static void main(String [] args){
+        Persona persona1=new Persona("Alejandro", 21);
+
+        persona1.setEdad(22);
+        persona1.mostrarDatos();
+    }
+}
